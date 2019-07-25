a550ab9e64c8d6a2cedfc9687ba2b16f11552305
boogy <theboogymater@gmail.com>
2019-07-25 17:06:13 +0200

    update

--- bin/update-git-tools.sh
+++ bin/update-git-tools.sh
@@ -4,7 +4,7 @@ for DIR in $(ls $HOME/tools); do
     if test -d $DIR; then
         cd $DIR
         if test -d .git; then
-            echo "[+] Running git pull in $DIR"
+            echo -en "\n\n[+] Running git pull in $DIR"
             git pull
         fi
     fi
