1365b1fcc5a4c87afec536bf6c450c7cf434550e
boogy <theboogymater@gmail.com>
2019-08-12 11:15:50 +0200

    i3 update && git update script

--- bin/update-git-tools.sh
+++ bin/update-git-tools.sh
@@ -4,7 +4,7 @@ for DIR in $(ls $HOME/tools); do
     if test -d $DIR; then
         cd $DIR
         if test -d .git; then
-            echo -en "\n\n[+] Running git pull in $DIR"
+            echo -en "\n\n[+] Running git pull in $DIR\n"
             git pull
         fi
     fi
