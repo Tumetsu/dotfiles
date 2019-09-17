d14f3cd4a1c0dc95f732f8c3b9d364c442341454
boogy <theboogymaster@gmail.com>
2019-09-17 09:29:24 +0200

    update i3 display

--- .config/i3/scripts/xinput-config.sh
+++ .config/i3/scripts/xinput-config.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-$exec xinput --set-prop "DLL06E4:01 06CB:7A13 Touchpad" "libinput Natural Scrolling Enabled" 0
-$exec xinput --set-prop "DLL06E4:01 06CB:7A13 Touchpad" "libinput Click Method Enabled" 0 1
-$exec xinput --set-prop "DLL06E4:01 06CB:7A13 Touchpad" 'libinput Accel Speed' 0.6
-$exec xinput --set-prop "Logitech Perforance MX" "Device Accel Constant Deceleration" 0.700000
-$exec xinput --set-prop "ZLY ZELOTES GAME MOUSE" "Device Accel Constant Deceleration" 0.700000
-$exec xinput --set-prop "pointer:Logitech MX Master 2S" "libinput Accel Speed" 1.000000
+xinput --set-prop "DLL06E4:01 06CB:7A13 Touchpad" "libinput Natural Scrolling Enabled" 0
+xinput --set-prop "DLL06E4:01 06CB:7A13 Touchpad" "libinput Click Method Enabled" 0 1
+xinput --set-prop "DLL06E4:01 06CB:7A13 Touchpad" 'libinput Accel Speed' 0.6
+xinput --set-prop "Logitech Perforance MX" "Device Accel Constant Deceleration" 0.700000
+xinput --set-prop "ZLY ZELOTES GAME MOUSE" "Device Accel Constant Deceleration" 0.700000
+xinput --set-prop "pointer:Logitech MX Master 2S" "libinput Accel Speed" 1.000000
+xinput --set-prop "SynPS/2 Synaptics TouchPad" "libinput Accel Speed" 1
