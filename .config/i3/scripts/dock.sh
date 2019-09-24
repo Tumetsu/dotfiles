bb6f8665fe266cb19ba1d93f4d82232369721850
boogy <theboogymaster@gmail.com>
2019-09-25 00:03:16 +0200

    i3 updates

--- .config/i3/scripts/dock.sh
+++ .config/i3/scripts/dock.sh
@@ -19,14 +19,11 @@ sleep 2
 # xrandr --auto
 if [ $displayCount -eq 3 ]; then
     echo "Enabling work dock"
-    $HOME/.config/i3/scripts/display 2
-    i3-msg "workspace 1; move workspace to DP2-1"
-    i3-msg "workspace 2; move workspace to DP1-2"
-    i3-msg "workspace 4; move workspace to DP2-1"
+    $HOME/.config/i3/scripts/display work
 
 elif [ $displayCount -eq 2 ]; then
     echo "Enabling home dock"
-    $HOME/.config/i3/scripts/display dual4k
+    $HOME/.config/i3/scripts/display home
 
 elif [ $displayCount -eq 1 ]; then
     echo "Enabling one display $intDisplay"
